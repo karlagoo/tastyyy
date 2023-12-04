1db568417ef24d0b521b6f2ec67721d2aac4b4a8
Karla Goo <karla_goo@yahoo.com>
2023-12-04 14:34:29 -0600

    created main components and pages

---
+++ src/components/Vegan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Vegan() {
+  return (
+    <div>Vegan component</div>
+  )
+}
+
+export default Vegan
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Popular from '../components/Popular';
+import Vegan from '../components/Vegan';
+
+function Home() {
+  return (
+    <div>
+    <Popular />
+    <Vegan />
+    </div>
+  )
+}
+
+export default Home
