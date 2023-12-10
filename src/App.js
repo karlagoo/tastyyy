0727105dadb97fe4991fae4fd67550d02a8a46de
Karla Goo <karla_goo@yahoo.com>
2023-12-10 14:45:16 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to tastyyy</h1>
+      <h1>Welcome to Tastyyy</h1>
+      <BrowserRouter>
+      <Category/>
       <Pages />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from "react-router-dom";
+
+function Cuisine() {
+
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=9&cuisine=${name}`);
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+    };
+
+    useEffect(() =>{
+        getCuisine(params.type);
+        console.log(params.type);
+    },[params.type])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Cuisine
