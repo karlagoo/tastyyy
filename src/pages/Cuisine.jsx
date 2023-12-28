49ecc3481567173b084f1548c4735b354d0eae99
Karla Goo <karla_goo@yahoo.com>
2023-12-28 10:49:30 -0600

    added motion transitions

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,7 +20,12 @@ function Cuisine() {
   }, [params.type]);
 
   return (
-    <Grid animate={{ opacity: 0 }}>
+    <Grid 
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit = {{ opacity: 0 }}
+      transition={{duration: 0.5}}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
